f75cded62ec4df82ef07a49d356f126c30ced76a
Ryuuzakira <ryuuzakira@gmail.com>
2014-10-10 15:23:18 -0300

    Update BatalhaNaval.java

--- BatalhaNaval.java
+++ BatalhaNaval.java
@@ -2,8 +2,5 @@ public class BatalhaNaval{
   public static void main(String[] args)
   {
     System.out.println("Bem Vindo ao jogo batalha Naval em Java");
-    
-    for(i = 0; i < 4; i++)
-      System.out.println("teste" + i "\n");
   }
 }
