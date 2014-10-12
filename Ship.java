af1165b1034e0a71d02533b69a15bec9c5f019c5
jcwillian <jcwillian.santos@gmail.com>
2014-10-12 15:21:15 -0300

    Change atribute hits to private
    Change atribute hits to private and add method getHits to return value of hits.

--- Ship.java
+++ Ship.java
@@ -2,7 +2,7 @@
  * Class Ship has atributes of each ship of game.
  */
 public class Navio{
-    boolean hits;           // boolean to know if ship was hit.
+    private boolean hits;           // boolean to know if ship was hit.
     
     // method construct to initialize hits as false.
     public Navio{
@@ -10,5 +10,10 @@ public class Navio{
     }
     public void acertar(){
         hits = true;
+    }
+    
+    // method that return a boolean hits.
+    public getHits(){
+    	return hits;
     }
 }
